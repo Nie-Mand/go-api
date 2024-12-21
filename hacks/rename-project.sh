6b53176ce8b0765e7593b3cb7acea30e29e2785c
Mohamed Sofiene (Niemand) <chill.out.x19@gmail.com>
2024-12-21 22:02:01 +0100

    chore: :bug: comment script

--- hacks/rename-project.sh
+++ hacks/rename-project.sh
@@ -12,7 +12,8 @@ if [ -z "$PROJECT_NAME" ]; then
     exit 0
 fi
 
-# Replace all occurrences of the default project name with the new project name
-find . -type f -exec sed -i "s,$DEFAULT_PROJECT_NAME,$PROJECT_NAME,g" {} +
+# TODO: Replace all occurrences of the default project name with the new project name
+# find . -path ./.git -prune -o -type f -exec sed -i "s,$DEFAULT_PROJECT_NAME,$PROJECT_NAME,g" {} +
+# -exec sed -i "s,$DEFAULT_PROJECT_NAME,$PROJECT_NAME,g" {} +
 
 echo "Done"
